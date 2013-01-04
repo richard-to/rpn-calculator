26bf348bfe42d191b9db69ecf2c1165956369517
richard-to <gohn76@gmail.com>
2013-01-03 17:41:34 -0900

    Remove workspace files from repo.

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -2,9 +2,8 @@
 //  CalculatorBrain.m
 //  Calculator
 //
-//  Created by CS193p Instructor.
-//  Copyright (c) 2011 Stanford University.
-//  All rights reserved.
+//  Created by Richard To on 12/28/12.
+//  Copyright (c) 2012 Richard To. All rights reserved.
 //
 
 #import "CalculatorBrain.h"
