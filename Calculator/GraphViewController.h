dbbc83dc7979579d16298270f5ca9a70a4302d66
richard-to <gohn76@gmail.com>
2013-01-03 19:23:47 -0900

    Add stub Graph View controller with segue

---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by Richard To on 1/3/13.
+//  Copyright (c) 2013 Richard To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
